bf25b51fad24e8c2570afc774ed4f500b4418984
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2022-04-28 14:34:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    return cats.push("Ralph");
+}
+// destructivelyAppendCat();
+
+function destructivelyPrependCat() {
+    return cats.unshift("Bob");
+}
+// destructivelyPrependCat();
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+// destructivelyRemoveLastCat();
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+// destructivelyRemoveFirstCat();
+
+function appendCat() {
+    let copyCats = [...cats, 'Broom'];
+    return copyCats;
+}
+// appendCat();
+
+function prependCat() {
+    const copyCats = ['Arnold', ...cats];
+    return copyCats;
+}
+// prependCat();
+
+function removeLastCat() {
+    const copyCats = [...cats];
+    copyCats.pop();
+    return copyCats;
+}
+// removeLastCat();
+
+function removeFirstCat() {
+    console.log(cats)
+    const copyCat = [...cats];
+    copyCat.shift();
+    return copyCat;
+}
+// removeFirstCat();
